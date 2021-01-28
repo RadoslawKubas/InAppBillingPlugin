efc7c46aebfcc3fe53a7bc78cc57f452f3e6274a
Radoslaw Kubas <rk@vrspot.es>
2021-01-28 15:13:27 +0100

    Revert "Android UpgradePurchasedSubscriptionAsync implementation"
    This reverts commit dcfaca8bc7feb32c6f44e1226d9668997ee62e9e.

--- src/Plugin.InAppBilling/InAppBilling.uwp.cs
+++ src/Plugin.InAppBilling/InAppBilling.uwp.cs
@@ -67,11 +67,6 @@ namespace Plugin.InAppBilling
             return xmlReceipt.ToInAppBillingPurchase(ProductPurchaseStatus.AlreadyPurchased);
         }
 
-        public override Task<InAppBillingPurchase> UpgradePurchasedSubscriptionAsync(string newProductId, string oldProductId, string purchaseTokenOfOriginalSubscription, int prorationMode = 1, IInAppBillingVerifyPurchase verifyPurchase = null)
-        {
-            throw new NotImplementedException("UWP not supported.");
-        }
-
         /// <summary>
         /// Purchase a specific product or subscription
         /// </summary>
